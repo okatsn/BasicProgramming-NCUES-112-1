2da063d04abc52362e4b9936391d63752b2da682
Documenter.jl <documenter@juliadocs.github.io>
2023-10-27 02:29:47 +0000

    build based on 4f276ae

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BasicProgrammingNCUES1121","category":"page"},{"location":"#BasicProgrammingNCUES1121","page":"Home","title":"BasicProgrammingNCUES1121","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BasicProgrammingNCUES1121.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BasicProgrammingNCUES1121]","category":"page"}]
+}
