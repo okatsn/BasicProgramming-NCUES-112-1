413e23e9a8bce6666457e16984ea668671086f39
okatsn <okatsn@gmail.com>
2023-11-17 09:54:37 +0000

    Finish Example lottery and save the output pdf

--- scripts/exam_lottery.jl
+++
@@ -1,9 +0,0 @@
-# Which exam will you take
-using CSV, DataFrames, Markdown
-using Random
-
-df = CSV.read("data/BasicProgrammingStudentList_112-1.csv", DataFrame)
-
-# Add password (only once)
-# insertcols!(df, :password => [randstring(rand(5:8)) for _ in 1:nrow(df)])
-# CSV.write("data/BasicProgrammingStudentList_112-1.csv", df)
