1e9037be5ebfad9e17203315ce1e47b6b3a0d17f
okatsn <okatsn@gmail.com>
2023-11-22 06:13:00 +0000

    score summary script

---
+++ scripts/score_summary.jl
@@ -0,0 +1,15 @@
+using CairoMakie, AlgebraOfGraphics
+using BasicProgrammingNCUES1121, DataFrames
+
+# ARGS = ["https://docs.google.com/spreadsheets/d/XXXX/edit?usp=sharing"]; # for example
+
+google_id = ARGS[1]
+
+cloudscore = readgsheet("https://docs.google.com/spreadsheets/d/$google_id/edit?usp=sharing", RawScore())
+prosheet!(cloudscore)
+
+plt = data(get_data(cloudscore)) * AlgebraOfGraphics.density() * mapping(:score) * mapping(layout=:Test_ID)
+
+f = draw(plt)
+
+Makie.save(projectdir("docs", "src", "score_distribution.png"), f)
