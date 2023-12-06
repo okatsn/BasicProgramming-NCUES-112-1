b9f67c77800b6cdf9c4d038cc781b9049e4b19e7
okatsn <okatsn@gmail.com>
2023-12-06 07:22:57 +0000

    Test table outputs

--- scripts/score_group.jl
+++ scripts/score_group.jl
@@ -13,18 +13,24 @@
 using CairoMakie, AlgebraOfGraphics #hide
 using BasicProgrammingNCUES1121, DataFrames#hide
 using Chain#hide
+using PrettyTables#hide
+using HypertextLiteral#hide
+using Suppressor#hide
 
 # # An example for ARGS: #src
 # ARGS = ["https://docs.google.com/spreadsheets/d/XXXX/edit?usp=sharing"]; #src
 # YOUR_GroupScore_Sheet_IDENTIFIEER = ARGS[1] #src
 
-cloudscore = readgsheet("https://docs.google.com/spreadsheets/d/YOUR_GroupScore_Sheet_IDENTIFIEER/edit?usp=sharing", GroupScore()) #hide
+# KEYNOTE: Suppressor is required otherwise google sheet url will be printed. #src
+cloudscore = @suppress readgsheet("https://docs.google.com/spreadsheets/d/YOUR_GroupScore_Sheet_IDENTIFIEER/edit?usp=sharing", GroupScore()) #hide
 # # In your local machine: #src
 # cloudscore = readgsheet(GroupScore()) #src
 
 # CHECKPOINT: No method supports prosheet yet #src
 # prosheet!(cloudscore) #src
 
+mdtable1(df) = pretty_table(df; backend=Val(:markdown), header=names(df)) #hide
+mdtable2(df) = @htl "$df" #hide
 score = cloudscore.data #hide
 score1 = select(score, #hide
     "電子郵件地址" => :Email, #hide
@@ -40,6 +46,7 @@ tb1 = @chain score1 begin #hide
     groupby(:Evaluator) #hide
     combine(nrow => "已評組員數") #hide
 end #hide
+mdtable1(tb1) #hide
 
 
 # ### 受評者摘要
@@ -48,3 +55,4 @@ tb2 = @chain score1 begin #hide
     groupby(:Evaluatee) #hide
     combine(nrow => "被評次數") #hide
 end #hide
+mdtable2(tb2) #hide
